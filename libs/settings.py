58f61afe6f0a7e870dd3ce68f5ac4dc39aa29ab4
ggllima <ggllima@usp.br>
2023-05-01 03:34:33 -0300

    chores: erase some tests

--- libs/settings.py
+++ libs/settings.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
 import sqlalchemy as sa
+import pandas as pd
+conn_rotten_engine = sa.create_engine('mssql://GUILHERME/rotten_tomatoes_top_movies?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server')
 
-rotten_engine = sa.create_engine('mssql://GUILHERME/rotten_tomatoes_top_movies?trusted_connection=yes&&driver=ODBC+Driver+17+for+SQL+Server')
